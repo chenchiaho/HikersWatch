4882d795f36cf17efe455359ec0e30ee9f3515f5
Kevin La <chenchiaho@gmail.com>
2021-05-26 20:51:54 +0800

    finalize string format

--- app/src/main/java/com/example/android/travelerswatch/MainActivity.kt
+++ app/src/main/java/com/example/android/travelerswatch/MainActivity.kt
@@ -2,14 +2,17 @@ package com.example.android.travelerswatch
 
 import android.content.Context
 import android.content.pm.PackageManager
+import android.location.Geocoder
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import java.lang.Exception
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,25 +26,31 @@ class MainActivity : AppCompatActivity() {
         locationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
         locationListener = object : LocationListener {
             override fun onLocationChanged(location: Location) {
-                Log.i("Location", location.toString())
+                updateLocationInfo(location)
             }
 
+            @Suppress("DEPRECATION")
             override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
                 super.onStatusChanged(provider, status, extras)
             }
 
-            override fun onProviderEnabled(provider: String) {
-                super.onProviderEnabled(provider)
-            }
-
-            override fun onProviderDisabled(provider: String) {
-                super.onProviderDisabled(provider)
-            }
         }
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 2)
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                2
+            )
         } else {
-            locationManager!!.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener!!)
+            locationManager!!.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                0,
+                0f,
+                locationListener!!
+            )
             val lastLocation = locationManager!!.getLastKnownLocation(LocationManager.GPS_PROVIDER)
             if (lastLocation != null) {
                 updateLocationInfo(lastLocation)
@@ -49,19 +58,65 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>,
-        grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
         if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                locationManager!!.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener!!)
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED) {
+                locationManager!!.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    0,
+                    0f,
+                    locationListener!!
+                )
             }
         }
     }
 
-
     fun updateLocationInfo(location: Location) {
 
+        var lat = findViewById<TextView>(R.id.lat)
+        var lon = findViewById<TextView>(R.id.lon)
+        var alt = findViewById<TextView>(R.id.alt)
+        var acc = findViewById<TextView>(R.id.accu)
+        var address = findViewById<TextView>(R.id.address)
+
+
+        lat.text = "Latitude:    ${String.format("%.4f", location.latitude)}"
+        lon.text = "Longitude: ${String.format("%.4f", location.longitude)}"
+        alt.text = "Altitude:     ${String.format("%.4f", location.altitude)}"
+        acc.text = "Accuracy:   ${String.format("%.2f", location.accuracy)}"
+
+        var addressString = "Could not find address"
+        val geocoder = Geocoder(this, Locale.getDefault())
+
+        try {
+            val list = geocoder.getFromLocation(location.latitude, location.longitude, 1)
+            if (list != null && list.isNotEmpty()) {
+                addressString = "Address:\n"
+                if (list[0].thoroughfare != null)
+                {
+                    addressString += list[0].thoroughfare + "\n"
+                }
+                if (list[0].locality != null)
+                {
+                    addressString += list[0].locality + " "
+                }
+                if (list[0].postalCode != null)
+                {
+                    addressString += list[0].postalCode + " "
+                }
+                if (list[0].adminArea != null) {
+                    addressString += list[0].adminArea
+                }
+            }
+        } catch (e: Exception) { e.printStackTrace()}
+        address.text = addressString
     }
 }
